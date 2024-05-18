02bb8f4e1fd59c81a6706d785adac6530ad9045c
Eric Sun <eric.sun@plu.edu>
2024-05-17 21:09:56 -0700

    bruh what am i doing

--- src/cat_model.ts
+++ src/cat_model.ts
@@ -10,15 +10,9 @@ let canvas : HTMLCanvasElement;
 let renderer : THREE.WebGLRenderer;
 let camera : THREE.PerspectiveCamera;
 let scene : THREE.Scene;
+let gui = new GUI;
 
 let capture = false;   // Whether or not to download an image of the canvas on the next redraw
-
-///////////////////////////////////////////////////////////
-///////////  Do not modify the code below /////////////////
-///////////////////////////////////////////////////////////
-
-const guiState = {
-};
 
 export const controller = {
     init,
@@ -37,33 +31,55 @@ function init() {
     camera.position.z = 2.5;
     renderer = new THREE.WebGLRenderer({ canvas });
 
+
     const texLoader = new THREE.TextureLoader();
     
-    const diffTex = texLoader.load( 'models/cat_head_body_texture.png' );
+    const diffTex = texLoader.load( 'models/cat_full_body_texture.png' );
     diffTex.flipY = false;
 
     const loader = new GLTFLoader().setPath('models/');
 
-    loader.load( 'cat_test.glb', function ( gltf ) {
+    
+
+    loader.load( 'cat_full_with_bones.glb', function ( gltf ) {
+        const model = gltf.scene;
         gltf.scene.traverse( ( child ) => {
             console.log(child.type);
-            if( child.type === 'Mesh' ) {
-                const mesh = child as THREE.Mesh;
-                mesh.material = new THREE.ShaderMaterial({ 
+            if( child.type === 'SkinnedMesh' ) {
+                const mesh = child as THREE.SkinnedMesh;
+                console.log(mesh.geometry, mesh.material, mesh.skeleton);
+                const brug = new THREE.SkinnedMesh(mesh.geometry, mesh.material);
+                brug.skeleton = mesh.skeleton;
+                brug.material = new THREE.ShaderMaterial({ 
                     uniforms: {
                         diffuseTex: { value: diffTex },
                     },
                     vertexShader: vertShader, 
                     fragmentShader: fragShader 
                 });
-                scene.add(child);
+
+                var helper = new THREE.SkeletonHelper(brug.skeleton.bones[0]);
+                helper.bones[0].updateMatrixWorld(true);
+
+                scene.add(brug);
+                scene.add(helper);
+
             }
         });
+        gui.add( guiState, "Spine", -180.0, 180.0 ).name("Body rotation: ")
+                .onChange(function(value: Number) {
+                    const bruhg = model.getObjectByName('cat_mesh') as THREE.SkinnedMesh;
+                    console.log(bruhg.skeleton);
+                    bruhg.skeleton.bones[0].rotation.y = Number(value);
+                    draw();
+                });
     }, undefined, function ( error ) {
         console.log("???");
         console.error( error );
 
     } );
+
+
 
     draw();
 }
@@ -95,7 +111,30 @@ const buttons = {
     screenshot: () => { capture = true; }
 };
 
+const guiState = {
+    Spine : 0,
+    Head : 0,
+    RightForeLeg : 0,
+    LeftForeLeg : 0,
+    RightHindLeg : 0,
+    LeftHindLeg : 0,
+    Tail : 0
+};
+
 function setupGui() {
-    const gui = new GUI();
+    // const gui = new GUI();
     gui.add(buttons, 'screenshot' ).name("Capture Screenshot");
+
+    
+    // gui.add( guiState, "angle2", -180.0, 180.0 ).name("Thigh rotation: ")
+    //     .onChange((v : number) => angleChanged("thigh", v));
+    // gui.add( guiState, "angle3", -180.0, 180.0 ).name("Shin rotation: ")
+    //     .onChange((v : number) => angleChanged("shin", v));
+    // gui.add( guiState, "angle4", -180.0, 180.0 ).name("Foot rotation: ")
+    //     .onChange((v : number) => angleChanged("foot", v));
+    // gui.add( guiState, "angle5", -180.0, 180.0 ).name("Big toe rotation: ")
+    //     .onChange((v : number) => angleChanged("bigToe", v));
+    // gui.add( guiState, "angle6", -180.0, 180.0 ).name("Other toes: ")
+    //     .onChange((v : number) => angleChanged("otherToes", v));
+
 }
