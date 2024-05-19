c527a95fc18eb7bcacc3fc3c4a7833a73de3ba41
Eric Sun <eric.sun@plu.edu>
2024-05-18 22:28:03 -0700

    I FINALLY GOT THE CAT TO MOVE PROPERLY

--- src/cat_model.ts
+++ src/cat_model.ts
@@ -3,15 +3,10 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import * as THREE from 'three'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-import vertShader from './shader/cat.vert.glsl';
-import fragShader from './shader/cat.frag.glsl';
-
 let canvas : HTMLCanvasElement;
 let renderer : THREE.WebGLRenderer;
 let camera : THREE.PerspectiveCamera;
 let scene : THREE.Scene;
-let gui = new GUI;
-
 let capture = false;   // Whether or not to download an image of the canvas on the next redraw
 
 export const controller = {
@@ -21,16 +16,13 @@ export const controller = {
 };
 
 function init() {
-    setupGui();
-
     scene = new THREE.Scene();
     canvas = document.getElementById('main-canvas') as HTMLCanvasElement;
     camera = new THREE.PerspectiveCamera( 48, canvas.width / canvas.height, 0.1, 1000 );
     const controls = new OrbitControls(camera, canvas);
     controls.enablePan = true;
-    camera.position.z = 2.5;
+    camera.position.z = 5;
     renderer = new THREE.WebGLRenderer({ canvas });
-
 
     const texLoader = new THREE.TextureLoader();
     
@@ -39,50 +31,30 @@ function init() {
 
     const loader = new GLTFLoader().setPath('models/');
 
-    
+    const light = new THREE.AmbientLight( 0xffffff ); // soft white light
+    scene.add( light );
 
     loader.load( 'cat_full_with_bones.glb', function ( gltf ) {
         const model = gltf.scene;
+        scene.add(model)
+        let mesh: THREE.SkinnedMesh<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.Material | THREE.Material[], THREE.Object3DEventMap>;
         gltf.scene.traverse( ( child ) => {
             console.log(child.type);
             if( child.type === 'SkinnedMesh' ) {
-                const mesh = child as THREE.SkinnedMesh;
-                console.log(mesh.geometry, mesh.material, mesh.skeleton);
-                const brug = new THREE.SkinnedMesh(mesh.geometry, mesh.material);
-                brug.skeleton = mesh.skeleton;
-                brug.material = new THREE.ShaderMaterial({ 
-                    uniforms: {
-                        diffuseTex: { value: diffTex },
-                    },
-                    vertexShader: vertShader, 
-                    fragmentShader: fragShader 
-                });
-
-                var helper = new THREE.SkeletonHelper(brug.skeleton.bones[0]);
-                helper.bones[0].updateMatrixWorld(true);
-
-                scene.add(brug);
-                scene.add(helper);
-
+                mesh = child as THREE.SkinnedMesh;
+                // var helper = new THREE.SkeletonHelper(model);
+                // scene.add(helper);
             }
         });
-        gui.add( guiState, "Spine", -180.0, 180.0 ).name("Body rotation: ")
-                .onChange(function(value: Number) {
-                    const bruhg = model.getObjectByName('cat_mesh') as THREE.SkinnedMesh;
-                    console.log(bruhg.skeleton);
-                    bruhg.skeleton.bones[0].rotation.y = Number(value);
-                    draw();
-                });
+        setupGui(model);
     }, undefined, function ( error ) {
         console.log("???");
         console.error( error );
-
     } );
-
-
 
     draw();
 }
+
 
 function draw() : void {
     renderer.render( scene, camera );
@@ -96,6 +68,7 @@ function draw() : void {
         aEl.click();
         aEl.remove();
     }
+
     window.requestAnimationFrame( draw );
 }
 
@@ -121,20 +94,54 @@ const guiState = {
     Tail : 0
 };
 
-function setupGui() {
-    // const gui = new GUI();
+function setupGui(model: THREE.Group<THREE.Object3DEventMap>) {
+    const gui = new GUI();
     gui.add(buttons, 'screenshot' ).name("Capture Screenshot");
 
     
-    // gui.add( guiState, "angle2", -180.0, 180.0 ).name("Thigh rotation: ")
-    //     .onChange((v : number) => angleChanged("thigh", v));
-    // gui.add( guiState, "angle3", -180.0, 180.0 ).name("Shin rotation: ")
-    //     .onChange((v : number) => angleChanged("shin", v));
-    // gui.add( guiState, "angle4", -180.0, 180.0 ).name("Foot rotation: ")
-    //     .onChange((v : number) => angleChanged("foot", v));
-    // gui.add( guiState, "angle5", -180.0, 180.0 ).name("Big toe rotation: ")
-    //     .onChange((v : number) => angleChanged("bigToe", v));
-    // gui.add( guiState, "angle6", -180.0, 180.0 ).name("Other toes: ")
-    //     .onChange((v : number) => angleChanged("otherToes", v));
+    gui.add( guiState, "Spine", -180.0, 180.0 ).name("Body rotation: ")
+        .onChange(function(value: Number) {
+            model.getObjectByName('Spine').rotation.z = Number(value) / 180.0 * Math.PI;
+        });
+    gui.add( guiState, "Head", -180.0, 180.0 ).name("Head rotation: ")
+        .onChange(function(value: Number) {
+            model.getObjectByName('Head').rotation.x = Number(value) / 180.0 * Math.PI;
+        });
+    gui.add( guiState, "RightForeLeg", -180.0, 180.0 ).name("RightForeLeg rotation: ")
+        .onChange(function(value: Number) {
+            const part = model.getObjectByName('RightForeLeg');
+            part.rotation.x = 0;
+            part.rotation.y = 0;
+            part.rotation.z = 0;
+            part.rotateZ(-0.56);
+            part.rotateX(Number(value) / 180.0 * Math.PI);
+        });
+    gui.add( guiState, "LeftForeLeg", -180.0, 180.0 ).name("LeftForeLeg rotation: ")
+        .onChange(function(value: Number) {
+            const part = model.getObjectByName('LeftForeLeg');
+            part.rotation.x = 0;
+            part.rotation.y = 0;
+            part.rotation.z = 0;
+            part.rotateZ(0.56);
+            part.rotateX(Number(value) / 180.0 * Math.PI);
+        });
+    gui.add( guiState, "RightHindLeg", -180.0, 180.0 ).name("RightHindLeg rotation: ")
+        .onChange(function(value: Number) {
+            const part = model.getObjectByName('RightHindLeg');
+            part.rotation.x = 0;
+            part.rotation.y = 0;
+            part.rotation.z = 0;
+            part.rotateZ(-0.56);
+            part.rotateX(Number(value) / 180.0 * Math.PI);
+        });
+    gui.add( guiState, "LeftHindLeg", -180.0, 180.0 ).name("LeftHindLeg rotation: ")
+        .onChange(function(value: Number) {
+            const part = model.getObjectByName('LeftHindLeg');
+            part.rotation.x = 0;
+            part.rotation.y = 0;
+            part.rotation.z = 0;
+            part.rotateZ(0.56);
+            part.rotateX(Number(value) / 180.0 * Math.PI);
+        });
 
 }
